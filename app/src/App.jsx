23ab234e6bde864e273d9710c2cf042d14499a7d
Funkmasta116 <64722925+Funkmasta116@users.noreply.github.com>
2025-09-09 22:57:03 +0700

    Add React starter (Vite)

---
+++ app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>🌌 Fractal Living Art</h1>
+      <p>Hello Pi Browser! This is the starter page.</p>
+    </div>
+  )
+}
+
+export default App
